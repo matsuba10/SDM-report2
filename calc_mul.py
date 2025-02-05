862323f14a93babc7aee81768b1da087a831274f
matsuba10 <matsuba.masaki.td@tut.jp>
2025-02-05 17:26:30 +0900

    テストパスするように変更しました

--- calc_mul.py
+++ calc_mul.py
@@ -3,22 +3,28 @@
 import re
                 
 def calc(A,B):
+
         ai=str(A)
         bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
+
+        p = re.compile('\d+$')
+
+        if p.fullmatch(ai) and p.fullmatch(bi):
+                a=int(ai)
+                b=int(bi)
+
+                if (0 < a < 1000) and (0 < b < 1000):
+                        valid = True
+                
                 else:
-                        valid=False
+                        valid = False
         else:
-                valid=False
+                valid = False
                 
         if valid:
-                ans=a*b
+                ans = a * b
                 return ans
+        
         else:
                 return -1
         
