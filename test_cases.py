1750997bcbb81fb2283939d4c7afe89d57000d03
matsuba10 <matsuba.masaki.td@tut.jp>
2025-02-06 08:42:14 +0900

    テストケースの番号付けを更新しました

--- test_cases.py
+++ test_cases.py
@@ -8,41 +8,41 @@ from calc_mul import calc
 class TestCalc (unittest.TestCase):
 
         #有効同値でのテスト
-        def test_Valid_values1(self):
+        def test_Valid_values01(self):
                 self.assertEqual(250000, calc(500, 500))
 
-        def test_Valid_values2(self):
+        def test_Valid_values02(self):
                 self.assertEqual(180000,calc(300, 600))
 
-        def test_Valid_values3(self):
+        def test_Valid_values03(self):
                 self.assertEqual(180000, calc(600, 300))
         
         #境界値付近でのテスト
-        def test_boundary_values1(self):
+        def test_boundary_values01(self):
                 self.assertEqual(1, calc(1, 1))
         
-        def test_boundary_values2(self):
+        def test_boundary_values02(self):
                 self.assertEqual(-1, calc(0, 1))
         
-        def test_boundary_values3(self):
+        def test_boundary_values03(self):
                 self.assertEqual(-1, calc(0, 0))
 
-        def test_boundary_values4(self):
+        def test_boundary_values04(self):
                 self.assertEqual(999, calc(999, 1))
         
-        def test_boundary_values5(self):
+        def test_boundary_values05(self):
                 self.assertEqual(-1, calc(1000, 1))
         
-        def test_boundary_values6(self):
+        def test_boundary_values06(self):
                 self.assertEqual(999, calc(1, 999))
 
-        def test_boundary_values7(self):
+        def test_boundary_values07(self):
                 self.assertEqual(-1, calc(-1, 999))
 
-        def test_boundary_values8(self):
+        def test_boundary_values08(self):
                 self.assertEqual(998001, calc(999, 999))
         
-        def test_boundary_values9(self):
+        def test_boundary_values09(self):
                 self.assertEqual(-1, calc(1000, 999))
         
         def test_boundary_values10(self):
@@ -52,27 +52,27 @@ class TestCalc (unittest.TestCase):
                 self.assertEqual(-1, calc(1000, 1000))
         
         #無効同値でのテスト
-        def test_Fail_values1(self):
+        def test_Fail_values01(self):
                 self.assertEqual(-1, calc(-100, 999))
         
-        def test_Fail_values2(self):
+        def test_Fail_values02(self):
                 self.assertEqual(-1, calc(5000, 4000))
 
         #その他(記号、小数点など)
-        def test_other_values1(self):
+        def test_other_values01(self):
                 self.assertEqual(-1, calc(None, 600))
         
-        def test_other_values2(self):
+        def test_other_values02(self):
                 self.assertEqual(-1, calc(500, None))
 
-        def test_other_values3(self):
+        def test_other_values03(self):
                 self.assertEqual(-1, calc('Z', 999))
         
-        def test_other_values4(self):
+        def test_other_values04(self):
                 self.assertEqual(-1, calc(999, 'A'))
         
-        def test_other_values5(self):
+        def test_other_values05(self):
                 self.assertEqual(-1, calc(0.1, 500))
         
-        def test_other_values6(self):
+        def test_other_values06(self):
                 self.assertEqual(-1, calc(500, 0.1))
